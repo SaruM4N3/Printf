c232057d83ff6dffe9fc95e01aa493470b1130f0
saruM4N3 <zakobajo@gmail.com>
2024-11-30 07:13:03 +0100

    Some modif, especially on the makefile

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,22 @@
 /*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 07:30:24 by zsonie            #+#    #+#             */
-/*   Updated: 2024/11/29 07:30:54 by zsonie           ###   ########.fr       */
+/*   Updated: 2024/11/30 07:11:54 by zsonie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+#include "libftprintf.h"
+
+int	ft_printf(const char *str, ...)
 {
-	
+	va_list ptr;
+
+	va_start(ptr,str);
+	while (*str)
+	{
+		ft_printf_parse();
+	}
+	va_end(ptr);
+
+	return (0);
 }
---
+++ printf/ft_printf_parse.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_parse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 07:31:36 by zsonie            #+#    #+#             */
+/*   Updated: 2024/11/30 07:11:09 by zsonie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_printf_parse(const char *, ...)
+{
+	
+}
+
+/*
+TODO:
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+*/
