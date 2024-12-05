7734d2ef5c5721a318d525198800c23cc1935e0a
saruM4N3 <zakobajo@gmail.com>
2024-12-05 05:07:40 +0100

    Finished

--- printf/ft_printf_parse.c
+++ printf/ft_printf_parse.c
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 07:31:36 by zsonie            #+#    #+#             */
-/*   Updated: 2024/12/05 05:01:11 by zsonie           ###   ########.fr       */
+/*   Updated: 2024/12/05 05:04:55 by zsonie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,16 +65,16 @@ static void	ft_putnbr_iter(int n, int fd, size_t *fsize)
 	}
 }
 
-void	ft_pointer_action( unsigned long args, int fd, size_t *fsize)
+void	ft_pointer_action(unsigned long args, int fd, size_t *fsize)
 {
 	if (args == 0)
 	{
 		(*fsize) += 5;
-		write (fd, "(nil)", 5);
+		write(fd, "(nil)", 5);
 		return ;
 	}
 	(*fsize) += 2;
-	write( 1, "0x", 2);
+	write(1, "0x", 2);
 	ft_putnbr_base(args, "0123456789abcdef", 1, fsize);
 }
 
@@ -91,9 +91,11 @@ int	ft_printf_action(char current, va_list args, size_t *fsize)
 	else if (current == 'u')
 		ft_putnbr_base(va_arg(args, unsigned int), "0123456789", 1, fsize);
 	else if (current == 'x')
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef", 1, fsize);
+		ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef", 1,
+			fsize);
 	else if (current == 'X')
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF", 1, fsize);
+		ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF", 1,
+			fsize);
 	else if (current == '%')
 	{
 		ft_putchar_fd('%', 1);
